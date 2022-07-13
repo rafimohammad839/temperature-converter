721fed3cc7b955eca90f3d25fab9b848652fec55
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-07-13 20:48:11 +0530

    Added a different behaviour on this branch

--- src/TempConvertor.js
+++ src/TempConvertor.js
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const TempConvertor = () => {
   const [resultTemp, setResultTemp] = useState("");
   const [inputVal, setInputVal] = useState("");
   const [toCelsius, setToCelsius] = useState(true);
+  const [showOnSecond, setShowOnSecond] = useState(true);
 
   const convertTemp = (e) => {
     const { value } = e.target;
+    if (!value.length) {
+      setResultTemp("");
+    }
     setInputVal(value);
-    if (!value.length) return;
-    const floatVal = parseFloat(value);
-    if (toCelsius) {
+    
+
+    if (e.target.className === "input-2" && showOnSecond === true) {
+      setShowOnSecond(false);
+    } else if (e.target.className === "input-1" && showOnSecond === false) {
+      setShowOnSecond(true);
+    }
+
+    
+  };
+
+  useEffect(() => {
+    if (!inputVal.length) return;
+    const floatVal = parseFloat(inputVal);
+    if (showOnSecond) {
+      // Converting into celsius
       const result = ((floatVal - 32) * 5) / 9;
       setResultTemp(result.toString());
     } else {
-      const result = (floatVal * 9) / 5 + 32;
+      // Converting into fahrenheit
+      const result = (floatVal * 9 / 5) + 32;
       setResultTemp(result.toString());
     }
-  };
+  }, [showOnSecond, inputVal])
 
   const toggleToCelsius = () => {
     setInputVal("");
@@ -33,16 +51,23 @@ const TempConvertor = () => {
           <input
             type="number"
             id="temp-1"
+            className="input-1"
             onChange={convertTemp}
-            value={inputVal}
+            value={`${showOnSecond ? inputVal : resultTemp}`}
           />
         </div>
         <div className="repeat-arrow">
-          <i className="fa-solid fa-repeat" onClick={toggleToCelsius}></i>
+          <i className="fa-solid fa-repeat"></i>
         </div>
         <div>
           <label htmlFor="temp-2">{toCelsius ? "Celsius" : "Fahrenheit"}</label>
-          <input type="number" id="temp-2" value={resultTemp} readOnly />
+          <input
+            type="number"
+            id="temp-2"
+            className="input-2"
+            value={`${showOnSecond ? resultTemp : inputVal}`}
+            onChange={convertTemp}
+          />
         </div>
       </div>
     </>
@@ -50,3 +75,9 @@ const TempConvertor = () => {
 };
 
 export default TempConvertor;
+
+/*
+  Input field 1 - value1 --> result
+        common - result && showOnSecondInput
+  Input field 2 - value2 --> result
+*/
